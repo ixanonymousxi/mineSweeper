08af25e0ff2371bf016a4a267f1260a8e455650d
Sarah W <louise562@aol.com>
2020-09-08 14:48:41 -0500

    Changed board populate to pass to tile instance. Added neighbor logic to tile class.

--- board.rb
+++ board.rb
@@ -3,11 +3,13 @@ require "byebug"
 
 class Board
 
+    attr_reader :board
+
     def initialize(size)
-        @board = populate_board(size*size)
+        @board = create_board(size*size)
     end
 
-    def populate_board(size)
+    def create_board(size)
         bombs_arr = []
         bombs = size/10
         not_bombs = size - bombs
@@ -25,9 +27,23 @@ class Board
                 rand_i = rand(bombs_arr.length)
                 value = bombs_arr[rand_i]
                 bombs_arr.delete_at(rand_i)
-                Tile.new(value)
+                value
+                #Tile.new(value,@board)
             end
         end
+    end
+
+    def populate_board
+        @board = @board.map do |row|
+            row.map do |val|
+                Tile.new(val,self)
+            end
+        end
+    end
+
+    def [](pos)
+        x,y = pos
+        @board[x][y]
     end
 
     def render
@@ -41,4 +57,13 @@ class Board
 end
 
 x = Board.new(9)
+#print x.board
+x.populate_board
+puts
+#print x.board
 x.render
+print x[[8,8]].value
+puts
+#print x[[8,8]].board.render
+puts
+print x[[8,8]].find_pos
--- tile.rb
+++ tile.rb
@@ -1,9 +1,12 @@
 class Tile
 
-    def initialize(bomb)
+    attr_reader :board
+
+    def initialize(bomb, board)
         @revealed = false
         @bombed = bomb
         @flagged = false
+        @board = board
     end
 
     def value
@@ -19,10 +22,84 @@ class Tile
         @revealed = true
     end
 
-    def neighbors
+    def find_pos
+        @board.board.each_with_index do |row, i|
+            if row.include?(self)
+                return [i,row.index(self)]
+            end
+        end
     end
 
-    def neighbor_bomb_count
+    def neighbors(pos)
+        x,y = pos
+        neighbors_pos_arr = [
+            [(x+1),y],
+            [(x+1),(y+1)],
+            [(x+1),(y-1)],
+            [(x-1),y],
+            [(x-1),(y+1)],
+            [(x-1),(y-1)],
+            [x,(y+1)],
+            [x,(y-1)]
+        ]
+        # neighbors_pos_arr = []
+
+        # if x == 0
+        #     neighbors_pos_arr = [
+        #     [(x+1),y],
+        #     [(x+1),(y+1)],
+        #     [(x+1),(y-1)],
+        #     [x,(y+1)],
+        #     [x,(y-1)]
+        # ]
+        # elsif x == board.length
+        #     neighbors_pos_arr = [
+        #     [(x-1),y],
+        #     [(x-1),(y+1)],
+        #     [(x-1),(y-1)],
+        #     [x,(y+1)],
+        #     [x,(y-1)]
+        # ]
+        # elsif y == 0
+        #     neighbors_pos_arr = [
+        #     [(x+1),y],
+        #     [(x+1),(y+1)],
+        #     [(x-1),y],
+        #     [(x-1),(y+1)],
+        #     [x,(y+1)]
+        # ]
+        # elsif y == board.length  
+        #     neighbors_pos_arr = [
+        #     [(x+1),y],
+        #     [(x+1),(y-1)]
+        #     [(x-1),y],
+        #     [(x-1),(y-1)],
+        #     [x,(y-1)]
+        # ]  
+        # else
+        #     neighbors_pos_arr = [
+        #     [(x+1),y],
+        #     [(x+1),(y+1)],
+        #     [(x+1),(y-1)],
+        #     [(x-1),y],
+        #     [(x-1),(y+1)],
+        #     [(x-1),(y-1)],
+        #     [x,(y+1)],
+        #     [x,(y-1)]
+        # ]
+        # end
+
+        # neighbors_pos_arr
+
+    end
+
+    def neighbor_bomb_count(pos,board)
+        bomb_count = 0
+        neighbors = self.neighbors(pos)
+        neighbors.each do |neighbor|
+            bomb_count += 1 if board[neighbor].value == "B"
+        end
+        bomb_count
     end
 
 end
